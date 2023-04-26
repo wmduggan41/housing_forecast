5489541ad9de72ff0923d7b722212395bb64715e
Wmduggan41 <wmduggan@gmail.com>
2023-04-26 17:57:32 -0400

    completed ETL to align with db connection

--- 00_scripts/02_process_data.R
+++ 00_scripts/02_process_data.R
@@ -1,27 +1,24 @@
-# Install packages and call libraries to create DB instance
-# install.packages("readxl")
-# install.packages("DBI")
-# install.packages("RSQLite")
-# library(readxl)
-# library(DBI)
-# library(RSQLite)
+# Create 3 connections to 1 database using dbConnect()
+# Replace "path/to/database.db" with the path and name you want to use for the database file. 
 
-# Read in dataset from "00_data" directory
-data <- read.csv("00_data/wmd_house.csv")
+# 1.0 Read train dataset from "00_data" directory ----
+train_data <- read.csv("00_data/train.csv")
+# Make sure the database file extension is ".db"
+con <- dbConnect(RSQLite::SQLite(), "00_data/house_data.db")
+# Write the train data frame to the database using dbWriteTable() function from DBI package:
+dbWriteTable(conn = con, name = "house_train_tbl", value = train_data, overwrite = TRUE)
 
-# Create a connection to the SQLite database using dbConnect()
-# Replace "path/to/database.sqlite" with the path and name you want to use for the database file. 
-# Make sure the file extension is ".sqlite".
-con <- dbConnect(RSQLite::SQLite(), "00_data/database.sqlite")
-# Write the data frame to the database using dbWriteTable() function from DBI package:
-dbWriteTable(conn = con, name = "house_tbl", value = data, overwrite = TRUE)
+# 2.0 Read in test dataset from "00_data" directory ----
+test_data <- read.csv("00_data/test.csv")
+# Write the test data frame to the database using dbWriteTable() function from DBI package:
+dbWriteTable(conn = con, name = "house_test_tbl", value = test_data, overwrite = TRUE)
+
+# 3.0 Create new database using values of interest ----
+key_data <- read.csv("00_data/wmd_house.csv")
+# Write the key data frame to the database using dbWriteTable() function from DBI package:
+dbWriteTable(conn = con, name = "key_tbl", value = key_data, overwrite = TRUE)
+
 # Close the database connection using dbDisconnect() function:
 dbDisconnect(con)
 
 
-#install.packages("skimr")
-library(skimr)
-data %>% skim()
-
-train_check <- read.csv("00_data/train.csv")
-train_check %>% skim()
