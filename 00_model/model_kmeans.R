da79322cb15a0f9fb6d02387965de32a0de599a0
Wmduggan41 <wmduggan@gmail.com>
2023-04-30 00:50:02 -0400

    working plot

--- 00_model/model_kmeans.R
+++ 00_model/model_kmeans.R
@@ -13,7 +13,7 @@ server_kmeans <- function(input, output) {
   
   # Render k-means plot
   output$kmeans_plot <- renderPlotly({
-    p <- plot_ly(
+    p <- ggplotly(
       data = kmeans_data(),
       x = ~OverallQual,
       y = ~SalePrice,
@@ -31,4 +31,46 @@ server_kmeans <- function(input, output) {
     p
   })
 }
+
+# library(tidyverse)
+# library(corrplot)
+# library(ggplot2)
+# library(reshape2)
+# library(caret)
+
+# Selecting target variables
+ktrain <- processed_key_tbl
+ktrain$Id <- NULL
+
+numeric_data <- ktrain %>%
+  select_if(is.numeric)
+
+set.seed(123) # Set the seed for reproducibility
+trainIndex <- createDataPartition(ktrain$SalePrice, 
+                                  p = 0.7, 
+                                  list = FALSE, 
+                                  times = 1)
+
+training_data <- ktrain[trainIndex, ]
+testing_data <- ktrain[-trainIndex, ]
+
+
+# Fit k-means model on the training data
+library(cluster)
+kmeans_model <- kmeans(training_data[, c("SalePrice", "price_sqft")], 
+                       centers = 5, 
+                       iter.max = 300, 
+                       nstart = 10)
+
+# Visualize the clusters
+library(plotly)
+plot_ly(x = training_data$SalePrice, 
+        y = training_data$price_sqft, 
+        color = kmeans_model$cluster, 
+        colors = "Set1", mode = "markers", 
+        marker = list(size = 8)) %>%
+
+  layout(xaxis = list(title = "Sale Price"), 
+         yaxis = list(title = "per sqft"), 
+         title = "K-Means Clustering of House Quality")
 
