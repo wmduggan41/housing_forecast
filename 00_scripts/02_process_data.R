01e87eda40e82f20da194dbb364b225b514a1ed3
Wmduggan41 <wmduggan@gmail.com>
2023-05-01 18:20:15 -0400

    refactored label changes correctly

--- 00_scripts/02_process_data.R
+++ 00_scripts/02_process_data.R
@@ -6,19 +6,17 @@ train_data <- read.csv("00_data/train.csv")
 # Make sure the database file extension is ".db"
 con <- dbConnect(RSQLite::SQLite(), "00_data/house_data.db")
 # Write the train data frame to the database using dbWriteTable() function from DBI package:
-dbWriteTable(conn = con, name = "house_train_tbl", value = train_data, overwrite = TRUE)
+dbWriteTable(conn = con, name = "train_data", value = train_data, overwrite = TRUE)
 
 # 2.0 Read in test dataset from "00_data" directory ----
 test_data <- read.csv("00_data/test.csv")
 # Write the test data frame to the database using dbWriteTable() function from DBI package:
-dbWriteTable(conn = con, name = "house_test_tbl", value = test_data, overwrite = TRUE)
+dbWriteTable(conn = con, name = "test_data", value = test_data, overwrite = TRUE)
 
 # 3.0 Create new database using values of interest ----
 key_data <- read.csv("00_data/wmd_house.csv")
 # Write the key data frame to the database using dbWriteTable() function from DBI package:
-dbWriteTable(conn = con, name = "key_tbl", value = key_data, overwrite = TRUE)
+dbWriteTable(conn = con, name = "key_data", value = key_data, overwrite = TRUE)
 
 # Close the database connection using dbDisconnect() function:
 dbDisconnect(con)
-
-
