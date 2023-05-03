0c65745d07ca39bced771aa31b8f4ad7a4a9c11d
Wmduggan41 <wmduggan@gmail.com>
2023-05-02 23:21:24 -0400

    adjusted to include train into db

--- 00_scripts/02_process_data.R
+++ 00_scripts/02_process_data.R
@@ -1,10 +1,12 @@
 # Create 3 connections to 1 database using dbConnect()
 # Replace "path/to/database.db" with the path and name you want to use for the database file. 
 
-# 1.0 Read train dataset from "00_data" directory ----
-train_data <- read.csv("00_data/train.csv")
 # Make sure the database file extension is ".db"
 con <- dbConnect(RSQLite::SQLite(), "00_data/house_data.db")
+
+# 1.0 Read train dataset from "00_data" directory ----
+train_data <- read.csv("00_data/train.csv")
+
 # Write the train data frame to the database using dbWriteTable() function from DBI package:
 dbWriteTable(conn = con, name = "train_data", value = train_data, overwrite = TRUE)
 
