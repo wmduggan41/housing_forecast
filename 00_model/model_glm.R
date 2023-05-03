1a3b6d55f500162d01a0a2702f1eaa347a40e823
Wmduggan41 <wmduggan@gmail.com>
2023-05-03 18:24:29 -0400

    completed glm model

--- 00_model/model_glm.R
+++ 00_model/model_glm.R
@@ -1,10 +1,10 @@
-generate_forecast_glmnet <- function(data, length_out = 365, seed = NULL, 
+generate_forecast_glmnet <- function(data = processed_data_tbl, length_out = 12, seed = NULL, 
                                      penalty = 1, mixture = 0.5) {
   
-  strain_tbl <- data %>% 
+  train_tbl <- data %>% 
     tk_augment_timeseries_signature()
   
-  sfuture_data_tbl <- data %>%
+  future_data_tbl <- data %>%
     tk_index() %>%
     tk_make_future_timeseries(length_out = length_out) %>%
     tk_get_timeseries_signature() 
@@ -18,22 +18,22 @@ generate_forecast_glmnet <- function(data, length_out = 365, seed = NULL,
     
     model <- linear_reg(mode = "regression") %>%
       set_engine(engine = "lm") %>%
-      fit.model_spec(total_sqft ~ ., data = strain_tbl %>% select(total_sqft, index.num))
+      fit.model_spec(price_sqft ~ ., data = train_tbl %>% select(price_sqft, index.num))
     
   } else {
     model <- linear_reg(mode = "regression", penalty = penalty, mixture = mixture) %>%
       set_engine("glmnet") %>%
-      fit.model_spec(total_sqft ~ ., data = strain_tbl %>% select(-date, -label_text, -diff))
+      fit.model_spec(price_sqft ~ ., data = train_tbl %>% select(-date, -label_text, -diff))
   }
   
   
   prediction_tbl <- predict(model, new_data = future_data_tbl) %>%
     bind_cols(future_data_tbl) %>%
     select(.pred, index) %>%
-    rename(total_sales = .pred, 
+    rename(price_sqft  = .pred, 
            date        = index) %>%
-    mutate(label_text = str_glue("Date: {date}
-                                 Revenue: {scales::dollar(total_sales)}")) %>%
+    mutate(label_text  = str_glue("Date: {date}
+                                  USD per sqft: {scales::dollar(price_sqft)}")) %>%
     add_column(key = "Prediction")
   
   output_tbl <- data %>%
@@ -46,7 +46,7 @@ generate_forecast_glmnet <- function(data, length_out = 365, seed = NULL,
 
 plot_prediction <- function(processed_train_tbl) {
   
-  selected_vars <- c("MoSold", "YrSold", "YearBuilt", "YearRemodAdd", "LotArea", "price_sqft", "SalePrice", "OverallCond", "OverallQual")
+  selected_vars <- c("price_sqft", "SalePrice")
   
   # Compute correlation matrix
   corr_matrix <- cor(processed_train_tbl[, selected_vars], use = "complete.obs")
@@ -55,9 +55,9 @@ plot_prediction <- function(processed_train_tbl) {
   corrplot(corr_matrix, type = "upper", method = "circle", tl.col = "black")
   
   # Plot SalePrice vs. MoSold with text labels
-  g <- training_data %>%
+  g <- train_tbl %>%
     
-    ggplot(aes(YearBuilt, price_sqft)) +
+    ggplot(aes(date, price_sqft)) +
     
     geom_line(color = "#2c3e50") +
     geom_point(size = 0.05) +
@@ -66,7 +66,7 @@ plot_prediction <- function(processed_train_tbl) {
     theme_tq() +
     expand_limits(y = 0) +
     scale_y_continuous(labels = scales::dollar_format()) +
-    labs(x = "Year Built", y = "USD Per SQFT")
+    labs(x = "Date", y = "USD Per SQFT")
   
   ggplotly(g, tooltip = "text")
   
